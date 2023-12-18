a485801ef381c9608f96ad278d64825a008cf64a
tabishcodes <tabioshsheikhcs@gmail.com>
2023-12-18 21:17:18 +0530

    Video 67: Quick Note: Upcoming Video; Video 67: Optional: Classes In Javascript

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Video: 66 => Array Introduction 
+Video: 67 => Array Introduction 
     In this video we have learned about the Push & Pop Array Methods. 
 */
 using System.Collections;
@@ -134,7 +134,7 @@ void display(char[] DisplayIt)
 }
 
 /*
-Video: 66 => Static vs Dynamic Arrays
+Video: 68 => Static vs Dynamic Arrays
     In this video we have learned that
         Static Arrays has a fixed or set size.
             lookup & push func has time complexity of O(1).
@@ -143,3 +143,34 @@ Video: 66 => Static vs Dynamic Arrays
             lookup func is O(1) but append func can be O(1) or O(n).
             insert & delete func has time complexity of O(n).
 */
+
+/*
+Video: 69 => Quick Note: Upcoming Video
+*/
+
+/*
+    Video: 69 =>
+*/
+
+int[] arr = new int[1];
+int[] arr1 = new int[1];
+
+if(arr == arr1){
+    Console.WriteLine(true);
+}
+else
+{
+    Console.WriteLine();
+}
+
+public class Car{
+
+    string color = "black";
+
+    public static void Main(string[] args)
+    {
+        Car myObj = new Car();
+
+        Console.WriteLine(myObj.color);
+    }
+}
--- obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c0e79fca2111b28a6746cdbfda51f8a9bc00cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
